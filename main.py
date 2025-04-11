4fe9bdf90965a2c998c183ecc1edf59241bfe0bd
ChristianEffner <ch.effner@t-online.de>
2025-04-11 11:18:44 +0200

    Erste Abfrage erfolgreich getestet

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
+import Datenbank
 from Datenbank import Datenbankverbindung
+from Datenbank import Datenabfragen
 
 def main():
     db_verbindung = Datenbankverbindung.Datenbankverbindung()
+    db_abfragen = Datenabfragen.Datenbankabfragen()
 
-    db_verbindung.connect()
-    db_verbindung.close_connection()
+
+    db_abfragen.aktiveDebitorenAbfragen()
+    #db_verbindung.close_connection()
 
 if __name__ == "__main__":
     main()
