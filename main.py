de255766fe45da2a17e38a70a3f34a043990b14d
ChristianEffner <ch.effner@t-online.de>
2025-04-16 10:58:25 +0200

    Funktionen für Diagrammerstellung getestet

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 from Datenbank.Datenbankverbindung import Datenbankverbindung
 from Datenbank.Datenabfragen import Datenbankabfragen
+from Datenvisualisierung.DiagrammeErstellen import DiagrammeErstellen
+
 
 def main():
 
 
-    db_verbindung = Datenbankverbindung()
     db_abfragen = Datenbankabfragen()
+    diagramme = DiagrammeErstellen()
 
+    diagramme.generiereDebitorenIMMsDiagramm(1500, 4000)
 
-    db_abfragen.holeAktiveDebitoren(['510808'])
-    db_abfragen.holeAktiveUser(['510808'])
+    #diagramme.generiereAktiveDebitorenDiagramm(1180, db_abfragen.holeAktiveDebitoren(['510808']))
+
+    #diagramme.generiereAktiveUserDiagramm(db_abfragen.holeAktiveDebitoren(['510808']), db_abfragen.holeAktiveUser(['510808']))
+
 
 
 if __name__ == "__main__":
